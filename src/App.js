56fa411583805b8217f5b8e4eaf8670f9ccfc318
Jeraldjolly883 <jeraldjolly883@gmail.com>
2025-01-07 22:59:25 +0530

    project completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AddStudent from './Components/AddStudent';
+import SearchStudent from './Components/SearchStudent';
+import ShoppingCard from './Components/ShoppingCard';
+
+function App() {
+  return (
+    <div>
+      <AddStudent />
+      <SearchStudent />
+      <ShoppingCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ShoppingCard.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+
+const ShoppingCard = () => {
+  return (
+    <div>
+
+      <div className="row">
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+        <div className="col col-12 col-sm-5 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+        <div class="card" style>
+  <img src="https://m.media-amazon.com/images/I/61ybeKQto8L._SY741_.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Casio Watch</h5>
+    <p class="card-text">1,695</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShoppingCard
